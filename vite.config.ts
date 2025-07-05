749d1250e7473f1783b7dfcf62147841e5b7061e
Saeed M farahani <135009134+saeedmfarahani@users.noreply.github.com>
2025-07-05 15:20:44 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, type ConfigEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig((env) => {
-  const alias = env.command === "build"
-    ? {
-      alias: {
-        "react-dom/server": "react-dom/server.node",
-      },
-    }
-    : undefined;
-  return {
+export default defineConfig({
     plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-    resolve: {
-      ...alias
-    }
   }
-});
+);
