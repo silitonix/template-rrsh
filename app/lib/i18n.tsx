ba0cb906fda3d3fab438245bf3548b7070cd923a
SaeedMFarahani <silitonix@gmail.com>
2025-07-10 00:24:21 +0330

    :recycle: improvements and refactor

--- app/lib/i18n.tsx
+++ app/lib/i18n.tsx
@@ -1,69 +1,92 @@
 import i18n from "i18next";
+import type { NavLinkProps, LinkProps, NavigateProps, To } from "react-router";
+import { NavLink, Link, Navigate } from "react-router";
 import { initReactI18next } from "react-i18next";
-import en from "~/localize/en";
 import { data, useParams } from "react-router";
 import { StateError } from "~/lib/error";
-import { NavLink } from "react-router";
-import { Link } from "react-router";
-import type { NavLinkProps } from "react-router";
-import type { LinkProps } from "react-router";
-import type { To } from "react-router";
+import { useLocation } from "react-router";
+import en from "~/localize/en";
+import { useNavigate } from "react-router";
 
 /* add new languages to the resources */
-const resources = { en };
-const lang_key = "vite-lang";
-const lang_default = "en";
+const lng_res = { en };
+const lng_key = "vite-lng";
+const lng_default = "en";
+const lng_supported = Object.keys(lng_res);
 
-function iaddress(to: To) {
-  const { lang } = useParams();
-  if (!lang || lang == lang_default) return to;
-  if (typeof to === "object" && to.pathname?.startsWith("/")) {
-    to.pathname = `${lang}${to.pathname}`;
-  } else if (typeof to === "string" && to.startsWith("/")) {
-    to = `/${lang}${to}`;
+function iUseLocation(code?: string) {
+  const loc = useLocation();
+  let pathname = loc.pathname;
+  pathname = pathname.replace(/^\/?[^\/]+/, code || "");
+  return { ...loc, pathname };
+}
+
+function iTo(to: To): To {
+  const { lng } = useParams();
+  let newTo: To = to;
+  if (lng === undefined || lng === lng_default) return to;
+  if (typeof newTo === "string") {
+    newTo = {
+      pathname: newTo,
+    };
   }
-  return to;
+  if (!newTo.pathname?.startsWith("/")) return to;
+  newTo.pathname = `/${lng}${newTo.pathname}`;
+  return newTo;
 }
 
 function ILink(props: LinkProps) {
-  const to = iaddress(props.to);
+  const to = iTo(props.to);
   return <Link {...props} to={to} />;
 }
-
+function INavigate(props: NavigateProps) {
+  const to = iTo(props.to);
+  return <Navigate {...props} to={to} />;
+}
 function INavLink(props: NavLinkProps) {
-  const to = iaddress(props.to);
+  const to = iTo(props.to);
   return <NavLink {...props} to={to} />;
 }
 
 export function initI18n() {
-  const { lang } = useParams();
+  const { lng } = useParams();
+  if (lng == lng_default) {
+    const navigate = useNavigate();
+    navigate(iUseLocation());
+  }
   if (typeof window == "undefined") return;
-  if (typeof lang === "undefined") {
-    i18n.changeLanguage(lang_default);
-    localStorage.setItem(lang_key, lang_default);
+  if (typeof lng === "undefined") {
+    i18n.changeLanguage(lng_default);
+    localStorage.setItem(lng_key, lng_default);
     return;
   }
 
   const current = localStorage.getItem("vite");
-  if (lang == current) return;
-  if (!Object.hasOwn(resources, lang)) {
+  if (lng == current) return;
+  if (!Object.hasOwn(lng_res, lng)) {
     throw new StateError(404);
   }
-  localStorage.setItem(lang_key, lang);
-  i18n.changeLanguage(lang);
+  localStorage.setItem(lng_key, lng);
+  i18n.changeLanguage(lng);
 }
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
-    resources,
+    resources: lng_res,
     lng:
       typeof window == "undefined"
-        ? lang_default
-        : localStorage.getItem(lang_key) || lang_default,
+        ? lng_default
+        : localStorage.getItem(lng_key) || lng_default,
     interpolation: {
       escapeValue: false, // react already safes from xss
     },
   });
 
-export { INavLink as NavLink, ILink as Link };
+export {
+  INavLink as NavLink,
+  ILink as Link,
+  INavigate as Navigate,
+  iUseLocation as useLocation,
+  lng_supported as i18nSupported,
+};
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,3 @@
 import { prefix, type RouteConfig } from "@react-router/dev/routes";
 import { flatRoutes } from "@react-router/fs-routes";
-export default prefix("/:lang?/",(await flatRoutes())) satisfies RouteConfig;
+export default prefix("/:lng?/", await flatRoutes()) satisfies RouteConfig;
