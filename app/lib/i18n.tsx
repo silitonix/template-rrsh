183ba386041b179a966d71580548e9ad4e18312e
Saeed M Farahani <silitonix@gmail.com>
2025-07-28 09:30:17 +0330

    :sparkles: added redirect and address convertor

--- app/lib/i18n.tsx
+++ app/lib/i18n.tsx
@@ -2,12 +2,10 @@ import i18n from "i18next";
 import type { NavLinkProps, LinkProps, NavigateProps, To } from "react-router";
 import { NavLink, Link, Navigate } from "react-router";
 import { initReactI18next } from "react-i18next";
-import { data, useParams } from "react-router";
+import { useParams } from "react-router";
 import { StateError } from "~/lib/error";
 import { useLocation } from "react-router";
 import en from "~/localize/en";
-import { useNavigate } from "react-router";
-import { replace } from "react-router";
 import { redirect } from "react-router";
 
 /* add new languages to the resources */
@@ -21,12 +19,20 @@ function iUseLocation(code?: string) {
   const loc = useLocation();
   if (lng === undefined) return loc;
   let pathname = loc.pathname;
+
   if (code && !Object.hasOwn(lng_res, code)) {
     throw new Error("Language does not exit");
   }
+
   pathname = pathname.replace(/^\/?[^\/]+$/, code ? `/${code}` : "/");
 
   return { ...loc, pathname };
+}
+
+function iAddress(url: string) {
+  const { lng } = useParams();
+  if (!url.startsWith("/")) return url;
+  url = `/${lng}${url}`.split("/").filter((s) => s).join('/');
 }
 
 function iTo(to: To): To {
@@ -39,12 +45,17 @@ function iTo(to: To): To {
     };
   }
   if (newTo.pathname?.startsWith("/")) {
-    newTo.pathname = `/${lng}${newTo.pathname}`;
+    newTo.pathname = iAddress(newTo.pathname);
     return newTo;
   }
   return to;
 }
 
+function iredirect(url: string, init?: number | ResponseInit) {
+  const to = iTo(url);
+  url = typeof to == "string" ? to : to.pathname ?? "";
+  return redirect(url, init);
+}
 function ILink(props: LinkProps) {
   const to = iTo(props.to);
   return <Link {...props} to={to} />;
@@ -98,4 +109,6 @@ export {
   INavigate as Navigate,
   iUseLocation as useLocation,
   lng_supported as i18nSupported,
+  iredirect as redirect
 };
+
